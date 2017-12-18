282ec865f1311553f54d9b3fb5e72ecc9e679e48
jinliangyu <jinliangyums@outlook.com>
2017-12-18 16:21:28 +0800

    write some front-end codes, add requirements.txt

--- Algos/Algos/settings.py
+++ Algos/Algos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=mbhlndaj1dj2#^_rg@rhnyyf=6b6xl&+ghd(ggdkx$$d1v#ez'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DataFactory',
 ]
 
 MIDDLEWARE = [
--- Algos/DataFactory/views.py
+++ Algos/DataFactory/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def data_factory(request):
+    return render(request, 'dashboard.html')
